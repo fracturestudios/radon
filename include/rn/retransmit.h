a3c659f2a823c1cf0faae6e3c6acb58d1a83086f
Dave Kilian <dave.kilian@gmail.com>
2015-06-24 16:17:52 -0700

    Spec retransmission queue

--- include/rn/retransmit.h
+++ include/rn/retransmit.h
@@ -1,4 +1,7 @@
 
+#include <QHash>
+#include <QList>
+
 // Tracks sender-side packets that have not yet been acknowledged.
 //
 // This object is a utility used by RnSocket.
@@ -12,6 +15,32 @@
 //
 class RnRetransmit
 {
-    // TODO
+public:
+    // Adds a packet to the retransmission queue
+    void add(RnHeader *packet, size_t size, int numTries, int timeout);
+
+    // Updates the time remaining for each item in the queue
+    void update(int elapsed);
+
+    // Returns packets for which the timeout has expired
+    QList<RnHeader*> timedOut();
+
+    // Resets the timeout for the given packet
+    void reset(uint32_t seq);
+
+    // Removes the given packet from the retransmission queue
+    void remove(uint32_t seq);
+
+private:
+    struct Item
+    {
+        RnHeader *packet;   // The packet payload to (re)send
+        size_t size;        // Size of the packet to resend, in bytes
+        int timeout;        // Total milliseconds to wait for an ACK
+        int numTries;       // Number of times to attempt to send packet
+        int remainingTime;  // Milliseconds remaining until next try
+    };
+
+    QHash<uint32_t, Item> m_items;
 };
 
